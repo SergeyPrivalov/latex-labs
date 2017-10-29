a6eed317d5eaf0aa75ba53d7db30a69e90b7f7f2
Ilya Sukhoplyuev <suhoy95@gmail.com>
2017-10-30 01:28:44 +0500

    3-я часть

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ mptopdf fig1.mp
 mptopdf myfig1.mp
 mptopdf fig2.mp
 mptopdf myfig2.mp
+# mptopdf fig3.mp
+asy script3.asy
+asy myscript.asy
+latex script5 && dvips script5.dvi && ps2pdf script5.ps
 
 # Просто так надо...
 pdflatex main.tex
